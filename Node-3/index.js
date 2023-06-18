6da325891b1683abe34f5f1f6baa9f232b5cdcd2
Kareem Ibrahem <kareemibrahem1999@outlook.com>
2023-06-18 18:56:34 +0300

    last version

--- Node-3/index.js
+++ Node-3/index.js
@@ -77,9 +77,23 @@ const convertPriceToCurrency = function (data, currencyCode) {
   return products;
 };
 
+const createProduct = async function (product) {
+  try {
+    const response = await axios.post(
+      "https://api.escuelajs.co/api/v1/products",
+      product
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Error:", error.message);
+    throw error;
+  }
+};
+
 const server = http.createServer(async (req, res) => {
-  if (req.url.startsWith("/products")) {
+  if (req.method === "GET" && req.url.startsWith("/products")) {
     const urlParams = new URL(req.url, `http://${req.headers.host}`);
+
     const currencyCode = urlParams.searchParams.get("CUR") || "USD";
     try {
       const [exchangeRate, productsData] = await Promise.all([
@@ -98,6 +112,61 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Internal Server Error");
     }
+  } else if (req.method === "POST" && req.url === "/products") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", async () => 
+    {
+      const validateProduct = function (product) {
+        if (!product.title || typeof product.title !== "string") {
+          throw new Error("Invalid product title");
+        }
+      
+        if (!product.price || typeof product.price !== "number" || product.price <= 0) {
+          throw new Error("Invalid product price");
+        }
+      
+        if (
+          !product.description ||
+          typeof product.description !== "string" ||
+          product.description.length < 10
+        ) {
+          throw new Error("Invalid product description");
+        }
+      
+        if (
+          !product.categoryId ||
+          typeof product.categoryId !== "number" ||
+          product.categoryId <= 0
+        ) {
+          throw new Error("Invalid product category ID");
+        }
+      
+        if (
+          !product.images ||
+          !Array.isArray(product.images) ||
+          product.images.length === 0
+        ) {
+          throw new Error("Invalid product images");
+        }
+    };
+      try {
+        
+        const product = JSON.parse(body);
+        validateProduct(product);
+        const createdProduct = await createProduct(product);
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(createdProduct));
+      } catch (error) {
+        console.log("Error:", error.message);
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad Request");
+      }
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
@@ -107,3 +176,4 @@ const server = http.createServer(async (req, res) => {
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
